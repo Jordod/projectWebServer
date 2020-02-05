7130aad3d39070ca14e11068befd45864f854c8a
Jordan Davy <jordodsbot@gmail.com>
2020-02-05 16:46:06 +0000

    Adding Firebase Dependencies

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const https = require('https');
 const fs = require('fs');
+const admin = require('firebase-admin');
+const serviceAccount = require("./creds.json");
 
 const options = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem')
 };
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://tud-4year-project-257618.firebaseio.com"
+});
+
+const store = admin.firestore();
+
+store.collection("transactions").listDocuments().then(
+  x => x[0].get().then(y => {
+    console.log(y.data());
+  })
+);
 
 https.createServer(options, function (req, res) {
   var chunks = [];
